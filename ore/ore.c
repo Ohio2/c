58b6d35548883c8d1126c4fa554ac4d9c0ec5796
Maksymilian Sęk <sekmartyna4@gmail.com>
2020-12-15 09:22:27 +0100

    ores gotta be updated

--- ore/ore.c
+++ ore/ore.c
@@ -8,11 +8,24 @@ int main()
 {
     /*Ore but it's core. Get it, hahaha. C (Programming language) + Ore 
     it's spelled <c-ore>*/
-    char df = $1;
-    char df2 = $2;
+    char df = %1;
     switch(df)
     {
-        case '-S' :
-            system("git clone aur.archlinux.org"&df2 );
+        case '-Sa' :
+        /*i know typing in -Sa can be difficult, but as i cannot do anything about it, it'll have to do*/
+            system("git clone aur.archlinux.org/%2 $HOME/.ore && cd %2 && makepkg -sic && cd - && rm -rf %2 && cd -");
+        case 'S' :
+            system("sudo pacman -S %2");
+        case 'R' : 
+            system("sudo pacman -R %2");
+        case 'H' :
+            printf("Ohio2's handy-dandy AUR Helper & Pacman Wrapper rewritten in C!");
+            system("pacman -H");
+        case 'V' :
+            printf("c.rev.1");
+        case 'Syu' :
+            system("sudo pacman -Syu");
+        case 'Sy' : 
+            system("sudo pacman -Sy");
     }
 }
